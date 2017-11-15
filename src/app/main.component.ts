41551d2b82c4432e5d993b757ff84c582e452a2c
Eik-S <eikem@posteo.de>
2017-11-15 17:26:32 +0100

    no twitter connection anymore from the frontend

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -1,36 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { TwitterApiService } from './twitter/twitter-api.service';
-
-import * as Twitter from 'twit';
-
-import { twitterAuth } from '../environments/environment';
-
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  providers: [ TwitterApiService]
 })
 
-export class MainComponent implements OnInit {
-
-  private client = new Twitter({
-    consumer_key: twitterAuth.consumer_key,
-    consumer_secret: twitterAuth.consumer_secret,
-    access_token: twitterAuth.access_token_key,
-    access_token_secret: twitterAuth.access_token_secret
-  });
+export class MainComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private twitterApiService: TwitterApiService,
   ) {}
 
-  ngOnInit(): void {
-    this.client.get('search/tweets', {q: 'banana', count: 100}, function( err, data, response){
-      console.log(data);
-    });
-  }
 }
