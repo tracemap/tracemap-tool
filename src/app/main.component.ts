3c96d158fa24b54fd6fc988c31792075c7c0fdf0
Eik-S <eikem@posteo.de>
2017-12-15 20:32:17 +0100

    First visualizations are workinggit status!

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, AfterViewInit, OnChanges, ViewChild, NgModule } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs/Observable';
 
 
 
@@ -7,13 +9,72 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  providers: []
+  providers: [ ApiService]
 })
 
-export class MainComponent {
+export class MainComponent implements AfterViewInit, OnChanges {
+    @ViewChild('d3Component') d3Component;
+    tracemapId: string;
+    tracemapData: object;
+    relations: object[];
 
-  constructor(
-    private route: ActivatedRoute
-  ) {}
+    constructor(
+        private route: ActivatedRoute,
+        private apiService: ApiService
+    ) {}
 
+    ngAfterViewInit(): void {
+        this.tracemapId = this.route.params["_value"]["pid"];
+
+        this.apiService.getTracemapData( this.tracemapId)
+            .then( tracemapData => {
+                this.tracemapData = tracemapData;
+                this.addGraphData();
+        });
+    }
+
+    addGraphData(): void {
+        console.log(this.tracemapData);
+        let graphData = {
+            "nodes": [],
+            "links": []
+        };
+
+        let source = {};
+        source['id'] = this.tracemapData['tweet_info'][this.tracemapId]['author'];
+        source['group'] = 3;
+        graphData['nodes'].push(source);
+
+        this.tracemapData['retweeters'].forEach(user => {
+            let node = {};
+            node['id'] = user;
+            node['group'] = 1;
+            graphData['nodes'].push(node);
+            console.log("Node added");
+
+        });
+        graphData['nodes'].forEach( source => {
+            if( source['id'] in this.tracemapData['followers']) {
+                this.tracemapData['followers'][source['id']].forEach( target => {
+                    let link = {};
+                    link['source'] = source['id'];
+                    link['target'] = target;
+                    link['value'] = 1;
+                    graphData['links'].push(link);
+                });
+            }
+        });
+        this.tracemapData['graphData'] = graphData;
+        this.d3Component.graphData = this.tracemapData['graphData'];
+        this.d3Component.render();
+        console.log(this.tracemapData['followers'])
+        console.log(this.tracemapData['graphData']['links'])
+    }
+
+  ngOnChanges(): void {
+    console.log("TM-ID: " + this.tracemapId);
+  }
+  addRelations(): void {
+
+  }
 }
