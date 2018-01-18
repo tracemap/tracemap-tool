dd21f7c78ceace501ad6b02729df50590dbe0ac8
Eik-S <eikem@posteo.de>
2018-01-18 12:09:50 +0100

    first unstyled user info on node click

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -12,6 +12,8 @@ export class UserComponent {
 
     userId: string;
     userInfo: object;
+    userImage: string;
+    accountAge: string;
 
     constructor(
         private apiService: ApiService,
@@ -28,8 +30,31 @@ export class UserComponent {
             this.userId = newUser;
             this.apiService.getUserInfo( this.userId).subscribe( response => {
                 this.userInfo = response[this.userId];
-                console.log( response[this.userId]);
+                this.userImage = "https://twitter.com/" + this.userInfo['screen_name'] + "/profile_image?size=original"
+                this.setAge();
+                console.log(this.userInfo);
+
             });
+        }
+    }
+
+    setAge(): void {
+        let now = Date.now()/1000;
+        let created = Date.parse(this.userInfo['created_at'])/1000;
+        console.log(now);
+        console.log(created);
+        let age = now - created;
+        console.log(age);
+        if(age > 365 * 24 * 60 * 60){
+            age = age / (365 * 24 * 60 * 60);
+            console.log(age);
+            this.accountAge = Number((age).toFixed(1)) + " Years"
+        } else if (age > 30 * 24 * 60 * 60){
+            age = age / (30 * 24 * 60 * 60);
+            this.accountAge = Number((age).toFixed(1)) + " Months"
+        } else {
+            age = age / (24 * 60 * 60);
+            this.accountAge = Number((age).toFixed(0)) + " Days"
         }
     }
 
