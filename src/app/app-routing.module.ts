0e4ab58dfdbabcfb6be02a755ece7ebf2b77177b
Eik-S <eikem@posteo.de>
2018-02-05 20:39:16 +0100

    new home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent} from './home-page/home-page.component';
-import { DescriptionVkComponent} from './home-page/description-vk.component';
 import { DescriptionTwitterComponent } from './home-page/description-twitter.component';
 
 import { MainComponent } from './main.component';
@@ -12,10 +11,7 @@ import { InfoComponent } from './info.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 const routes: Routes = [
-	{ path: 'homepage', component: HomePageComponent , children: [
-		{path: 'vk', component: DescriptionVkComponent},
-		{path: 'twitter', component: DescriptionTwitterComponent}
-	]},
+	{ path: 'homepage', component: HomePageComponent},
 	{ path: '', redirectTo: 'homepage', pathMatch: 'full'},
     { path: 'twitter/:pid', component: MainComponent, children: [
     	{path: 'details/:uid', component: UserComponent},
