37d5f19daf51812dfa058231d6eb441a07f43f13
Eik-S <eikem@posteo.de>
2018-03-17 15:55:29 +0100

    canvas rendering finished, render by time not working now, will be fixed soon

--- src/app/tool-main-panel/d3.component.ts
+++ src/app/tool-main-panel/d3.component.ts
@@ -45,6 +45,7 @@ export class D3Component{
     hoveredNode;
     activeNode;
 
+    colors = ["151,41,255","29,161,242"]
     scale = d3.scaleLinear().domain([2, 30]).range([6, 18]);
 
     constructor(
@@ -52,19 +53,16 @@ export class D3Component{
         private comService: MainCommunicationService,
         private highlightService: HighlightService
     ){ 
-        // this.comService.userNodeHighlight.subscribe( userId => {
-        //     this.highlightNodeById(userId);
-        // });
         // this.comService.resetUserNodeHighlight.subscribe( userId => {
         //     this.resetHighlightNode();
         // });
-        // this.highlightService.highlight.subscribe( area => {
-        //     if( area == "graph") {
-        //         this.highlight = "highlight"
-        //     } else {
-        //         this.highlight = "";
-        //     }
-        // })
+        this.highlightService.highlight.subscribe( area => {
+            if( area == "graph") {
+                this.highlight = "highlight"
+            } else {
+                this.highlight = "";
+            }
+        })
     }
 
     ticked() {
@@ -103,32 +101,13 @@ export class D3Component{
         this.context.stroke();
 
         this.context.beginPath();
-        this.graphData.nodes.filter(node => {
-           if(node.group == 1 && node.opacity == 1) {
-               return node;
+        this.graphData.nodes.forEach(node => {
+           if(node.group == 1) {
+               this.drawNode(node);
+           } else {
+               this.drawAuthor(node);
            } 
-        }).forEach( node => this.drawNode(node));
-        this.context.fill();
-        this.context.stroke();
-
-        this.context.beginPath();
-        this.graphData.nodes.filter(node => {
-           if(node.group == 1 && node.opacity == 0.2) {
-               return node;
-           } 
-        }).forEach( node => this.drawNode(node));
-        this.context.fill();
-        this.context.stroke();
-
-        this.context.beginPath();
-        this.graphData.nodes.filter(node => {
-            if(node.group == 0) {
-                return node;
-            }
-        }).forEach( node => this.drawAuthor(node));
-        this.context.fill();
-        this.context.stroke();
-
+        });
     }
 
     resizeCanvas() {
@@ -141,26 +120,14 @@ export class D3Component{
         this.canvas.height = this.height;
         this.context = this.canvas.getContext("2d");
         this.simulation.stop()
-        this.setSimulation(this.simulateBy).then( (simulation) => {
+        this.setSimulation().then( (simulation) => {
             this.context.beginPath();
             this.simulation = simulation;
         });
     }
 
-    render() {
-        this.comService.userInfo.subscribe( userId => {
-            if( userId) {
-                this.graphData.nodes.forEach( node => {
-                    if( node.id_str == userId) {
-                        this.activeNode = node;
-                        console.log(this.activeNode);
-                    }
-                })
-            } else {
-                this.activeNode = undefined;
-            }
-        });
-
+    render( sim="default") {
+        this.simulateBy = sim;
 
         this.width = $('.canvas').width();
         this.height = $('.canvas').height();
@@ -171,36 +138,42 @@ export class D3Component{
         this.graphData.nodes.forEach( node => {
             node.r = this.scale(this.getNeighbours(node, "out")) * 1.6;
             node.opacity = 1.0;
+            node.color = 0;
         });
         this.graphData.links.forEach( link => {
             link.opacity = 1.0;
         });
-        this.setSimulation(this.simulateBy).then( (simulation) => {
+        this.addTimestamps();
+        this.setSimulation().then( (simulation) => {
             this.simulation = simulation;
-            d3.select(this.canvas)
-                .call( d3.drag()
-                    .container(this.canvas)
-                    .subject( () => { return node_hovered(this.canvas)})
-                    .on("start", () => { return this.dragstarted() })
-                    .on("drag", () => { return this.dragged()})
-                    .on("end", () => { return this.dragended()}));
-
+            if(this.simulateBy == "default") {
+                d3.select(this.canvas)
+                    .call( d3.drag()
+                        .container(this.canvas)
+                        .subject( () => { return node_hovered(this.canvas)})
+                        .on("start", () => { return this.dragstarted() })
+                        .on("drag", () => { return this.dragged()})
+                        .on("end", () => { return this.dragended()}));
+            } else {
+                d3.select(this.canvas)
+                    .on(".drag",null);
+            }
             d3.select(this.canvas)
                 .on("mousemove", () => {
                     let node = node_hovered(this.canvas);
                     if( node && !this.hoveredNode ) {
                         this.hoveredNode = node;
-                        this.highlightNeighbours(node);
+                        this.comService.userNodeHighlight.next(node.id_str);
                     } else if ( !node && this.hoveredNode && !this.dragging ){
-                        this.hoveredNode = undefined;
-                        this.resetHighlightNeighbours();
+                        this.comService.userNodeHighlight.next(undefined);
                     }
                 })
                 .on("click", () => {
                     let node = node_hovered(this.canvas);
-                    if( node && node != this.activeNode) {
+                    if( node) {
+                        this.activeNode = node;
                         this.comService.userInfo.next(node.id_str);
-                    } else if (!node && this.activeNode) {
+                    } else if (!node) {
                         this.comService.userInfo.next(undefined);
                     }
                 });
@@ -214,8 +187,52 @@ export class D3Component{
             }
         });
 
+        this.comService.userInfo.subscribe( userId => {
+            if( userId) {
+                this.graphData.nodes.forEach( node => {
+                    if( node.id_str == userId) {
+                        this.hoveredNode = undefined;
+                        this.activeNode = node;
+                        this.ticked();
+                    }
+                })
+            } else {
+                this.activeNode = undefined;
+                this.ticked();
+            }
+        });
+        this.comService.userNodeHighlight.subscribe( userId => {
+            if( userId) {
+                this.graphData.nodes.forEach( node => {
+                    if( node.id_str == userId) {
+                        this.hoveredNode = node;
+                        this.highlightNeighbours(node);
+                        this.ticked();
+                    }
+                });
+            } else {
+                this.resetHighlightNeighbours();
+                this.hoveredNode = undefined;
+                this.ticked();
+            }
+        }); 
+
     }
 
+    addTimestamps() {
+        this.graphData.nodes.forEach( node => {
+            node.timestamp = Date.parse(node.retweet_created_at) / 1000;
+        });
+        this.graphData.nodes.sort( (a,b) => {
+            return a.timestamp - b.timestamp;
+        })
+        let firstTime = this.graphData.nodes[0].timestamp;
+        let lastTime = this.graphData.nodes[this.graphData.nodes.length -1].timestamp;
+        let multiplicator = (lastTime - firstTime) / (this.width);
+        this.graphData.nodes.forEach( node => {
+            node.rel_timestamp = (node.timestamp - firstTime) / multiplicator;
+        })
+    }
 
 
     dragstarted() {
@@ -243,43 +260,76 @@ export class D3Component{
 
 
     drawLink(d) {
+        let angle = Math.atan2( d.target.y- d.source.y, d.target.x - d.source.x);
+        let xPos = d.target.x - d.target.r * Math.cos(angle);
+        let yPos = d.target.y - d.target.r * Math.sin(angle);
         this.context.moveTo(d.source.x, d.source.y);
-        this.context.lineTo(d.target.x, d.target.y);
+        this.context.lineTo( xPos, yPos);
         this.context.strokeStyle = "rgba(140,140,140,"+d.opacity+")";
         this.context.lineWidth = 1;
+        this.drawHead(xPos, yPos, angle)
+    }
+
+    drawHead(xPos, yPos, angle) {
+        let headlen = 5;
+        let headRightX = xPos - headlen * Math.cos(angle - Math.PI/6);
+        let headRightY = yPos - headlen * Math.sin(angle - Math.PI/6); 
+        this.context.lineTo( headRightX, headRightY);
+        this.context.moveTo( xPos, yPos);
+        let headLeftX = xPos - headlen * Math.cos(angle + Math.PI/6);
+        let headLeftY = yPos - headlen * Math.sin(angle + Math.PI/6); 
+        this.context.lineTo( headLeftX, headLeftY);
+
     }
 
     drawNode(d) {
-        let lineWidth = 3.5;
+        if( d == this.activeNode || d == this.hoveredNode) {
+            d.color = 1;
+        } else {
+            d.color = 0;
+        }
+        let lineWidth = d.r / 7;
         let radius = d.r - lineWidth;
+        this.context.beginPath();
         this.context.moveTo( d.x + radius, d.y)
         this.context.arc(d.x, d.y, radius, 0, 2*Math.PI);
         this.context.lineWidth = lineWidth;
-        this.context.fillStyle = "rgba(151,41,255,"+d.opacity+")";
-        this.context.strokeStyle = "rgba(151,41,255,"+d.opacity+")";
+        this.context.fillStyle = "rgba("+this.colors[d.color]+","+d.opacity+")";
+        this.context.strokeStyle = "rgba("+this.colors[d.color]+","+d.opacity+")";
+        this.context.fill();
+        this.context.stroke();
     }
 
     drawAuthor(d) {
+        if( d == this.activeNode || d == this.hoveredNode) {
+            d.color = 1;
+        } else {
+            d.color = 0;
+        }
         let lineWidth = d.r / 7;
         let radius = d.r - lineWidth;
+        this.context.beginPath();
         this.context.moveTo( d.x + radius, d.y)
         this.context.arc(d.x, d.y, radius, 0, 2*Math.PI);
         this.context.moveTo( d.x + radius / 2, d.y)
         this.context.arc(d.x, d.y, radius / 2, 0, 2*Math.PI);
         this.context.lineWidth = lineWidth;
         this.context.fillStyle = "#fff";
-        this.context.strokeStyle = "rgba(151,41,255,"+d.opacity+")";
+        this.context.strokeStyle = "rgba("+this.colors[d.color]+","+d.opacity+")";
+        this.context.fill();
+        this.context.stroke();
     }
 
-    setSimulation( sim:string): Promise<d3.forceSimulation> {
+    setSimulation(): Promise<d3.forceSimulation> {
         return new Promise( (resolve, reject) => {
-            this.simulateBy = sim;
             if( this.simulateBy == "default") {
                 this.simulateDefault().then( (simulation) => {
                     resolve(simulation);
                 });
             } else if( this.simulateBy == "time") {
-                this.simulateTime();
+                this.simulateTime().then( (simulation) => {
+                    resolve(simulation);
+                });
             }
         });
     }
@@ -318,41 +368,42 @@ export class D3Component{
             simulation.force("link")
                 .links(this.graphData.links);
             resolve(simulation);
-        })
+        });
 
     }
 
-    simulateTime() {
-        this.graphData.nodes.forEach( node => {
-            if( node.group == 0) {
-                node.fy = this.height / 2;
-            }
-            node.fx = node.rel_timestamp + 100;
-        })
+    simulateTime(): Promise<d3.forceSimulation> {
+        return new Promise( (resolve, reject) => {
+            this.graphData.nodes.forEach( node => {
+                if( node.group == 0) {
+                    node.fy = this.height / 2;
+                }
+                node.fx = node.rel_timestamp + 50;
+            });
+            let simulation = d3.forceSimulation()
+                .force("link", d3.forceLink().id(function(d) {
+                    return d.id_str; 
+                }).distance(function(d) {
+                    let sourceRad = d.source.r;
+                    let targetRad = d.target.r;
+                    return (sourceRad + targetRad);
+                }))
+                .force("charge", d3.forceManyBody().strength(function(d) {
+                    return d.r * -20;
+                }))
+                .force("collide", d3.forceCollide().radius(function(d) {
+                    return d.r * 1.5;
+                }))
+                .force("center", d3.forceCenter(this.width / 2, this.height / 2));
 
-        this.simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function(d) {
-                return d.id_str; 
-            }).distance(function(d) {
-                let sourceRad = d.source.r;
-                let targetRad = d.target.r;
-                return sourceRad + targetRad;
-            }))
-            .force("collide", d3.forceCollide().radius(function(d) {
-                return d.r * 1.5;
-            }).strength(1))
-            .force("charge", d3.forceManyBody().strength(function(d) {
-                return d.r * -30;
-            }));
+            simulation
+                .nodes( this.graphData.nodes)
+                .on( "tick", () => { return this.ticked()});
 
-        this.simulation
-            .nodes( this.graphData.nodes)
-            .on( "tick", () => { return this.ticked()});
-
-        this.simulation.force("link")
-            .links(this.graphData.links);
-
-        d3.selectAll(".d3-graph circle").on(".drag", null);
+            simulation.force("link")
+                .links(this.graphData.links);
+            resolve(simulation);
+        });
     }
 
     highlightNeighbours( node, direction=undefined) {
@@ -360,6 +411,7 @@ export class D3Component{
         let children;
         let parentLinks;
         let parents;
+
 
         //set graphs opacity to 0.2
         this.graphData.nodes.forEach( node => {
@@ -421,64 +473,21 @@ export class D3Component{
         this.ticked();
     }
 
-    highlightHover( node) {
-        let circle = d3.select(node);
-        this.hoveredNode = circle;
-        circle.classed("hovered", true);
-    }
-
-
-
-    highlightActive( node) {
-        let circle = d3.select( node);
-        this.activeNode = circle;
-        circle.classed("active", true);
-    }
-
-    highlightNodeById( userId, active=undefined) {
-        d3.selectAll('.d3-graph circle').filter( (node, index, circleArr) => {
-            if( node['id_str'] == userId){
-                if(active){
-                    this.highlightActive( circleArr[index]);
-                } else {
-                    this.highlightHover( circleArr[index]);
-                    this.highlightNeighbours( circleArr[index]);
+    getNeighbours(d, direction=undefined): number {
+        let matches = [];
+        this.graphData.links.forEach( link => {
+            if( !direction || direction == "in") {
+                if(link.target == d.id_str) {
+                    matches.push(link);
                 }
             }
-        });
-    }
-
-    resetHighlightNode() {
-        if( this.hoveredNode ) {
-            this.hoveredNode.classed("hovered", false);
-            this.resetHighlightNeighbours();
-        }
-    }
-
-    resetActiveNode() {
-        if( this.activeNode) {
-            this.activeNode.classed("active", false);
-        }
-    }
-
-
-    getNeighbours(d, direction=""): number {
-        let degree = 0;
-        for( let i in this.graphData.links) {
-            let source = this.graphData.links[i].source;
-            let target = this.graphData.links[i].target;
-            let check = true;
-            if( direction === "out") {
-                check = ( source === d.id_str);
-            } else if( direction === "in") {
-                check = ( target === d.id_str);
-            } else {
-                check = ( source === d.id_str || target === d.id_str);    
+            if(!direction || direction == "out") {
+                if(link.source == d.id_str) {
+                    matches.push(link);
+                }
             }
-            if( check)
-                degree += 1;
-        }
-        return degree;
+        })
+        return matches.length;
     }
 
     
