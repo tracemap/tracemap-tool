74bf375c40147fe8711e39da199f3eb9ebe14828
Eik-S <eikem@posteo.de>
2018-05-20 22:13:28 +0200

    first time slider version

--- src/app/tool/main-panel/timeslider.component.ts
+++ src/app/tool/main-panel/timeslider.component.ts
@@ -1,15 +1,36 @@
 import { Component, AfterViewInit } from '@angular/core';
 
 import * as $ from 'jquery';
+
 @Component({
     selector: 'timeslider',
     templateUrl: './timeslider.component.html',
     styleUrls: ['./timeslider.component.scss']
 })
 
-export class TimesliderComponent {
-    position = 0;
-    leftOffset = 0;
-    rightOffset = 
+export class TimesliderComponent implements AfterViewInit {
+    circlePos = 0;
+    width;
 
+    ngAfterViewInit() {
+        this.width = $('.slider').width();
+    }
+
+    moveHandle(event) {
+        this.circlePos = event.clientX - 356 - 60 - 60 - 10;
+    }
+
+    slideHandle(event) {
+        document.onmousemove = (event2) => {
+            this.circlePos = event2.clientX - 356 - 60 - 60 - 10;
+            if(this.circlePos > this.width) {
+                this.circlePos = this.width
+            } else if (this.circlePos < 0){
+                this.circlePos = 0;
+            }
+        };
+        document.onmouseup = (event3) => {
+            document.onmousemove = null;
+        }
+    }
 }
