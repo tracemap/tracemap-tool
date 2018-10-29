458329e628d0ae30cdce9b6fdc74aa73d29d8635
Eik-S <eikem@posteo.de>
2018-10-29 14:51:47 +0100

    progress bar implemented

--- src/app/tool/main-panel/progress-bar.component.ts
+++ src/app/tool/main-panel/progress-bar.component.ts
@@ -14,8 +14,8 @@ export class ProgressBarComponent {
     users: string[];
     uncrawledUsers: string[];
     unwrittenUsers: string[];
-    progressBarValue = 50;
     active = false;
+    finished = false;
 
     constructor(
         private communicationService: CommunicationService,
@@ -39,15 +39,21 @@ export class ProgressBarComponent {
         this.active = true;
         TimerObservable
         .create(5000, 5000)
-        .takeWhile(() => this.active)
+        .takeWhile(() => !this.finished)
         .subscribe(() => {
-            this.apiService.labelUnknownUsers(this.uncrawledUsers.concat(this.unwrittenUsers)).subscribe( response => {
+            this.apiService.labelUnknownUsers(
+                    this.uncrawledUsers.concat(this.unwrittenUsers)
+                    ).subscribe( response => {
                 this.unwrittenUsers = response['unwritten'];
                 this.uncrawledUsers = response['uncrawled'];
                 if (this.unwrittenUsers.concat(this.uncrawledUsers).length === 0) {
-                    this.active = false;
+                    this.finished = true;
                 }
             });
         });
+    }
+
+    reload(): void {
+        location.reload();
     }
 }
