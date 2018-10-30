0d9df702c9fd2f26df226f1fc21caf458a6c0531
Eik-S <eikem@posteo.de>
2018-10-30 16:11:37 +0100

    fixed update and deactivate progress bar if new tracemap is generated.

--- src/app/tool/main-panel/progress-bar.component.ts
+++ src/app/tool/main-panel/progress-bar.component.ts
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/takeWhile';
 })
 
 export class ProgressBarComponent {
+    tweetId: string;
     users: string[];
     uncrawledUsers: string[];
     unwrittenUsers: string[];
@@ -33,10 +34,18 @@ export class ProgressBarComponent {
                 });
             }
         });
+        this.communicationService.tweetId.subscribe( tweetId => {
+            if (this.tweetId !== tweetId) {
+                this.active = false;
+                this.finished = true;
+                this.tweetId = tweetId;
+            }
+        });
     }
 
     checkMissingUsers(): void {
         this.active = true;
+        this.finished = false;
         TimerObservable
         .create(5000, 5000)
         .takeWhile(() => !this.finished)
