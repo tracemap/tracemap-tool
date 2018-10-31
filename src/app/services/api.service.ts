efba40bf653296d90ff080c4abf1413620afabc7
Eik-S <eikem@posteo.de>
2018-10-31 15:41:56 +0100

    changed api calls to post requests

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,11 +8,14 @@ import { environment } from '../../environments/environment';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/observable/forkJoin';
+import { UserService } from './user.service';
 
 @Injectable( )
 
 export class ApiService {
     url = environment.apiUrl;
+    email: string;
+    sessionToken: string;
 
 
     tracemapData = new BehaviorSubject<object>(undefined);
@@ -23,14 +26,29 @@ export class ApiService {
     });
 
     constructor(
-        private http: Http
-        ) {
+        private http: Http,
+        private userService: UserService
+    ) {
+        this.userService.credentials.subscribe( credentials => {
+            if (credentials) {
+                this.email = credentials['email'];
+                this.sessionToken = credentials['session_token'];
+            }
+        });
     }
+
+    // Tool Methods
 
     getTweetInfo( tweetId: string): Observable<object> {
         console.log('#apiService#: getTweetInfo');
+        const url = this.url + '/twitter/get_tweet_info';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            tweet_id: tweetId
+        });
         return this.http
-            .get( this.url + '/twitter/get_tweet_info/' + tweetId)
+            .post( url, body, {headers: this.jsonHeader})
             .map( response => {
                 const data = response.json();
                 return data['response'];
@@ -39,8 +57,14 @@ export class ApiService {
 
     getTweetData( tweetId: string): Observable<object> {
         console.log('#apiService#: getTweetData');
+        const url = this.url + '/twitter/get_tweet_data';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            tweet_id: tweetId
+        });
         return this.http
-            .get( this.url + '/twitter/get_tweet_data/' + tweetId)
+            .post( url, body, {headers: this.jsonHeader})
             .map( response => {
                 const data = response.json();
                 return data['response'];
@@ -49,42 +73,42 @@ export class ApiService {
 
     getTimeline( userId: string): Observable<object> {
         console.log('#apiService#: getTimeline');
+        const url = this.url + '/twitter/get_user_timeline';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            user_id: userId
+        });
         return this.http
-            .get( this.url + '/twitter/get_user_timeline/' + userId)
-            .map( response => {
-                return response.json();
-            });
+            .post( url, body, {headers: this.jsonHeader})
+            .map( response => response.json());
     }
 
     getUserInfo( userId: string): Observable<object> {
         console.log('#apiService#: getUserInfo');
+        const url = this.url + '/twitter/get_user_info';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            user_id: userId
+        });
         return this.http
-            .get( this.url + '/twitter/get_user_info/' + userId)
-            .map( response => {
-                const data = response.json();
-                return data['response'];
-            });
-    }
-
-    getRetweeters( tweetId: string): Observable<string[]> {
-        console.log('#apiService#: getRetweeters');
-        return this.http
-            .get(this.url + '/twitter/get_retweeters/' + tweetId)
-            .map( response => {
-                const data = response.json();
-                return data['response'];
-            });
+            .post( url, body, {headers: this.jsonHeader})
+            .map( response => response.json());
     }
 
     getFollowers( retweeterIds: string[], authorId: string): Observable<object> {
         console.log('#apiService#: getFollowers');
-        const retweetersString = authorId + ',' + retweeterIds.toString();
+        retweeterIds.push(authorId);
+        const url = this.url + '/neo4j/get_followers';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            user_ids: retweeterIds
+        });
         return this.http
-            .get(this.url + '/neo4j/get_followers/' + retweetersString)
-            .map( response => {
-                const data = response.json();
-                return data['response'];
-            });
+            .post( url, body, {headers: this.jsonHeader})
+            .map( response => response.json());
     }
 
     getTracemapData(tweetId: string): Promise<object> {
@@ -100,6 +124,7 @@ export class ApiService {
                 }).subscribe( followersList => {
                     tracemapData['followers'] = followersList;
                     this.tracemapData.next(tracemapData);
+                    console.log(tracemapData);
                     resolve( tracemapData);
             });
         });
@@ -107,13 +132,18 @@ export class ApiService {
 
     labelUnknownUsers( retweeterIds: string[]): Observable<string[]> {
         console.log('#apiService#: labelUnknownUsers');
-        const retweetersString = retweeterIds.toString();
+        const url = this.url + '/neo4j/label_unknown_users';
+        const body = JSON.stringify({
+            email: this.email,
+            session_token: this.sessionToken,
+            user_ids: retweeterIds
+        });
         return this.http
-            .get(this.url + '/neo4j/label_unknown_users/' + retweetersString)
-            .map( response => {
-                return response.json();
-            });
+            .post( url, body, {headers: this.jsonHeader})
+            .map( response => response.json());
     }
+
+    // Nontool Methods
 
     addToNewsletter( email: string): Observable<string> {
         console.log('#apiService#: addToNewsletter()');
